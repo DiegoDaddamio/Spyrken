6d43cc13ced2f58793e53fb3b23520cc4e869f97
DiegoDaddamio <102663162+DiegoDaddamio@users.noreply.github.com>
2025-07-31 19:40:59 +0200

    Update exempl.py

--- exempl.py
+++ exempl.py
@@ -1,23 +1,24 @@
 from spyrken import *
 
-# Création du circuit et des noeuds (équipotentiels)
+# Création du circuit
 circuit = Circuit()
 
+# Création des noeuds (équipotentiels)
 gnd = circuit.add_node("gnd",ground=True)
 n1 = circuit.add_node("n1")
 n2 = circuit.add_node("n2")
 v_source = circuit.add_node("V_source")
 
-# Configuration d'un circuit linéaire
-R1 = Resistor(1, "R1")
+# Elements du circuit
+R1 = Resistor(100, "R1")
 C1 = Capacitor(1e-6, "C1")  
 L1 = Inductor(10e-3,"L1")
 V = VoltageSource(12, 1560,0 ,name="V") 
 
-#Rajout des composants dans le circuit
+# Rajout des composants dans le circuit
 circuit.add_component([V,R1,C1,L1])
 
-#Configuration des noeuds
+# Configuration des connections entre noeuds (from,to)
 V.connect(v_source, gnd)
 R1.connect(v_source, n1)
 L1.connect(n1,n2)
@@ -28,3 +29,4 @@ circuit.solve()
 
 # Affichage des résultats
 circuit.display()
+voltage_phasors(circuit)
